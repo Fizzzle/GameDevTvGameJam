a19f0d85a75ac17c8ad71b0e0721635bb84cfa86
Sedjex <kathshem@gmail.com>
2023-05-22 15:21:37 +0300

    Basic model Lvl1

---
+++ GameDevTvJam/Assets/Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private bool rotatingClockwise = false;
+
+	void Update () {
+	    if (rotatingClockwise && (transform.eulerAngles.z > 270 || transform.eulerAngles.z <= 0))
+	    {
+            transform.Rotate(0, 0, -1);
+	    }
+	    else if (!rotatingClockwise && transform.eulerAngles.z > 0)
+	    {
+            transform.Rotate(0, 0, 1);
+        
+        }
+
+        if (Input.anyKeyDown)
+	    {
+	        rotatingClockwise = !rotatingClockwise;
+	    }
+	}
+}
